31cce77327b5be1c2d10699956e8bb6e4b711a45
Eduardo Wanderley de Siqueira Araújo <wanderley.eduardo@gmail.com>
2024-06-13 14:31:38 -0300

    Associate Pins With Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_13_144315) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_13_171318) do
   create_table "pins", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
